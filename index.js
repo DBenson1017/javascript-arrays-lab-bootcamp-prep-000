00449d10613725bed405a7e475b8adafa0de97cf
Devin Benson <devinbenson@gmail.com>
2020-03-02 23:24:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(name){
-  return kittens.push(name)
+  return kittens.push(name) //changes underlying array
 }
 function destructivelyPrependKitten(name){
-  return kittens.unshift(name)
+  return kittens.unshift(name) //opposite of above 
 }
 function destructivelyRemoveLastKitten(name){
   return kittens.pop(name)
@@ -14,7 +14,8 @@ function destructivelyRemoveFirstKitten(name){
   return kittens.shift(name)
 }
 function appendKitten(name){
-  var newKittens = kittens.concat(name)
+  var newKittens = kittens.concat(name) 
+  // makes a new array (newKittens) that is the old array with a name on the end 
 return newKittens
 }
 function prependKitten(name){
@@ -22,9 +23,11 @@ return [name,...kittens]
 }
 function removeLastKitten(){
   return kittens.slice(0, kittens.length-1)
+  //returns all but the final element, doesn't really 'remove' it 
   
 }
 function removeFirstKitten(){
   return kittens.slice(1)
+  //returns the full array after slicing i 0to1
 }
 
